e5a9ce5b49c2381a8fcbd394e661b6737221785e
Manickavasagam <manickavasagam.a@sysnetsystem.in>
2025-10-24 15:41:26 +0530

    Add VendorFetchLogRepository interface

---
+++ src/main/java/com/example/murion/v10/Repository/VendorFetchLogRepository.java
@@ -0,0 +1,10 @@
+package com.example.murion.v10.Repository;
+
+import com.example.murion.v10.Entity.VendorFetchLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VendorFetchLogRepository extends JpaRepository<VendorFetchLog, Long> {
+    Optional<VendorFetchLog> findByVendorName(String vendorName);
+}
