8bd5c067f57a01ddddc546e76e535169adbb3e6b
Manickavasagam <manickavasagam.a@sysnetsystem.in>
2025-10-24 16:10:18 +0530

    Update Application.java

--- src/main/java/com/example/murion/v10/Application.java
+++ src/main/java/com/example/murion/v10/Application.java
@@ -2,6 +2,7 @@ package com.example.murion.v10;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
