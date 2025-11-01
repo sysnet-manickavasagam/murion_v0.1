d734e627a24bdcab3c7853c5a3cd43e2ff5e7b41
Manickavasagam <manickavasagam.a@sysnetsystem.in>
2025-11-01 19:10:00 +0530

    Change API endpoint mapping for SecurityCheckController

--- src/main/java/com/example/murion/v10/Controller/SecurityCheckController.java
+++ src/main/java/com/example/murion/v10/Controller/SecurityCheckController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
-@RequestMapping("/api/security")
+@RequestMapping("/api")
 public class SecurityCheckController {
 
     @Autowired
